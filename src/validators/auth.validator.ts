c2db61c48ce742d3a3c7c1b3f4c446b6fdee9349
nirman2 <nirman.khanal@oratechnologies.io>
2025-12-21 13:02:13 +0545

    made changes according to code review

--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -11,9 +11,9 @@ export const merchantQuickRegisterSchema = z.object({
     .min(8, 'Password must be at least 8 characters')
     .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
     .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
-    .regex(/[0-9]/, 'Password must contain at least one number'),
+    .regex(/[0-9]/, 'Password must contain at least one number')
+    .max(20, 'Password must not exceed 20 characters.'),
   name: z.string().min(2, 'Name must be at least 2 characters'),
-  // businessName: z.string().min(2, 'Business name must be at least 2 characters'),
   phone: z.string().optional(),
 });
 
